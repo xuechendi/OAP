04308b43f8a64d631535379c7aa1c1e2cd7fb26c
Chendi Xue <chendi.xue@intel.com>
2020-08-14 14:30:11 +0800

    [oap-native-sql] Fallback to use rowBased Join when key contains expression
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>

--- oap-native-sql/core/src/main/scala/com/intel/oap/expression/ConverterUtils.scala
+++ oap-native-sql/core/src/main/scala/com/intel/oap/expression/ConverterUtils.scala
@@ -19,6 +19,7 @@ package com.intel.oap.expression
 
 import java.io.ByteArrayInputStream
 import java.nio.ByteBuffer
+import java.util.ArrayList
 import com.intel.oap.vectorized.ArrowWritableColumnVector
 import io.netty.buffer.ArrowBuf
 import org.apache.spark.rdd.RDD
@@ -30,7 +31,10 @@ import org.apache.arrow.vector.ipc.message.{
   MessageSerializer,
   IpcOption
 }
+import org.apache.arrow.vector.types.pojo.Field
 import org.apache.arrow.vector.types.pojo.Schema
+import org.apache.arrow.gandiva.expression._
+import org.apache.arrow.gandiva.evaluator._
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.expressions.aggregate._
@@ -44,6 +48,7 @@ import scala.collection.JavaConverters._
 import scala.collection.mutable.ListBuffer
 import io.netty.buffer.{ByteBuf, ByteBufAllocator, ByteBufOutputStream}
 import java.nio.channels.{Channels, WritableByteChannel}
+import com.google.common.collect.Lists
 
 object ConverterUtils extends Logging {
   def createArrowRecordBatch(columnarBatch: ColumnarBatch): ArrowRecordBatch = {
@@ -258,6 +263,21 @@ object ConverterUtils extends Logging {
           tmpAttr
         }
     }
+  }
+
+  def getColumnarFuncNode(expr: Expression): TreeNode = {
+    if (expr.isInstanceOf[AttributeReference] && expr
+          .asInstanceOf[AttributeReference]
+          .name == "none") {
+      throw new UnsupportedOperationException(
+        s"Unsupport to generate native expression from replaceable expression.")
+    }
+    var columnarExpr: Expression =
+      ColumnarExpressionConverter.replaceWithColumnarExpression(expr)
+    var inputList: java.util.List[Field] = Lists.newArrayList()
+    val (node, _resultType) =
+      columnarExpr.asInstanceOf[ColumnarExpression].doColumnarCodeGen(inputList)
+    node
   }
 
   def ifEquals(left: Seq[AttributeReference], right: Seq[NamedExpression]): Boolean = {
